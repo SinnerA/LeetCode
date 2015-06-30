6b351f8c06532827406461d57a25a0c304e3a434
SinnerA <wu181184@sina.com>
2015-06-30 19:40:44 +0800

    2015-06-30

--- 025.Reverse Nodes in k-Group.cpp
+++ 025.Reverse Nodes in k-Group.cpp
@@ -26,7 +26,7 @@ Tags: Linked List
  * };
  */
 
-/*每次选取好一个group后,调用翻转链表子函数翻转该group*/
+/*方法一：每次选取好一个group后,调用翻转链表子函数翻转该group*/
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
@@ -49,6 +49,8 @@ public:
 		}
 		return tmpHead.next;
     }
+	
+	//翻转链表,并返回翻转后链表的头结点和尾节点
 	pair<ListNode*, ListNode*> reverseList(ListNode* head, ListNode* end){
 		ListNode *p = head, *pre = NULL;
 		while(p != end){
@@ -58,5 +60,42 @@ public:
 			p = tmp;
 		}
 		return make_pair(pre, head);
+	}
+};
+
+
+/*方法二：上面的方法实际上遍历了2遍链表,可以稍加改进在计数k个链表的过程中就开始翻转, 最后部分如果不足k个节点,则还原原来的顺序*/
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode tmpHead(0);
+		ListNode *p = head, *tail = &tmpHead;
+		while(p){
+			pair<ListNode*, ListNode*> pair = reverseList(p, k);
+			tail->next = pair.first;
+			tail = pair.second;
+			p = tail->next;
+		}
+		return tmpHead.next;
+    }
+	
+	//翻转从head开始的k个链表节点，如果不足k个则不翻转，返回翻转后的这k个节点
+    //子链表的head和tail。翻转后的子链表还是和子链表后面的部分相连的。
+	pair<ListNode*, ListNode*> reverseList(ListNode* head, int k){
+		ListNode *pre = NULL, *p = head;
+		int cnt = 0;
+		while(p && cnt < k){
+			ListNode *tmp = p->next;
+			p->next = pre;
+			pre = p;
+			p = tmp;
+			cnt++;
+		}
+		head->next = p;                   //把翻转后的子链表和子链表后面的部分连起来
+		if(cnt == k){
+			return make_pair(pre, head);
+		} else {
+			return reverseList(pre, cnt); //不足k个，还原原来的顺序
+		}
 	}
 };
