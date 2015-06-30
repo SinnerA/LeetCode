e8be288838e6407a2efba9db02676db2fb857a6d
SinnerA <wu181184@sina.com>
2015-06-30 08:52:53 +0800

    finish

--- 138.Copy List with Random Pointer.cpp
+++ 138.Copy List with Random Pointer.cpp
@@ -14,6 +14,17 @@ Tags: Hash Table Linked List
  *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
  * };
  */
+ 
+/*
+算法1：我们在构建新链表的节点时，保存原始链表的next指针映射关系，并把指针做如下变化(蓝色为原始链表节点，紫红色为新链表节点)：
+参考：http://www.cnblogs.com/TenosDoIt/p/3387000.html
+
+然后在上图的基础上进行如下两步
+1、构建新链表的random指针：比如new1->random = new1->random->random->next, new2->random = NULL, new3-random = NULL, new4->random = new4->random->random->next
+2、恢复原始链表：根据最开始保存的原始链表next指针映射关系恢复原始链表
+
+该算法时间空间复杂度均为O(N)
+*/
 class Solution {
 public:
     RandomListNode *copyRandomList(RandomListNode *head) {
@@ -31,7 +42,7 @@ public:
 			preOld->next = pnew;
 			pnew = new RandomListNode(pold->label);
 			preNew->next = pnew;
-			pnew->random = preOld;
+			pnew->random = pold;
 			
 			preOld = pold;
 			preNew = pnew;
@@ -57,3 +68,14 @@ public:
 		return newHead;
     }
 };
+
+/*
+算法2：该算法更为巧妙，不用保存原始链表的映射关系，构建新节点时，指针做如下变化，即把新节点插入到相应的旧节点后面：
+参考：http://www.cnblogs.com/TenosDoIt/p/3387000.html
+
+同理分两步
+1、构建新节点random指针：new1->random = old1->random->next, new2-random = NULL, new3-random = NULL, new4->random = old4->random->next
+2、恢复原始链表以及构建新链表：例如old1->next = old1->next->next,  new1->next = new1->next->next
+
+该算法时间复杂度O(N)，空间复杂度O(1)
+*/
