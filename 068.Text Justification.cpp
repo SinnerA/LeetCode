1d94ae1767f89c345b5f03eb37927fcc919c7a87
SinnerA <wu181184@sina.com>
2015-07-06 22:44:58 +0800

    2015-07-06

--- 068.Text Justification.cpp
+++ 068.Text Justification.cpp
@@ -28,9 +28,87 @@ In this case, that line should be left-justified.
 Tags: String
 */
 
+/*
+这一题需要注意两个点：
+a、当该行只放一个单词时，空格全部在右边 
+b、最后一行中单词间只有一个空格，其余空格全部在右边。然后只要贪心选择，在一行中尽量放多的单词。  
+*/
 class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        
+		vector<string> res;
+		if(words.empty()) return res;
+		int n = words.size(), i = 0;
+		
+		while(i < n){
+			int rowLen = 0;
+			string rowStr = "";
+			
+			int j = i;
+			while(j < n && rowLen + words[j].size() <= maxWidth){
+				rowLen += (words[j++].size() + 1);
+			}
+			
+			if(j - i == 1){
+				rowStr = words[i];
+				addSpace(rowStr, maxWidth - rowStr.size());
+				res.push_back(rowStr);
+				i = j;
+				continue;
+			}
+			
+			int charLen = rowLen - (j - i);
+			int meanSpace = (j < n) ? (maxWidth - charLen) / (j - i - 1) : 1;
+			int leftSpace = (j < n) ? (maxWidth - charLen) % (j - i - 1) : maxWidth - charLen - (j - i -1);
+			for(int k = i; k < j - 1; k++){
+				rowStr += words[i];
+				addSpace(rowStr, meanSpace);
+				if(leftSpace){
+					addSpace(rowStr, 1);
+					leftSpace--;
+				}
+			}
+			rowStr += words[j - 1];
+			
+			if(leftSpace){
+				addSpace(rowStr, leftSpace);
+			}
+			res.push_back(rowStr);
+			i = j;
+		}
+		return res;
     }
+	
+	void addSpace(string &s, int cnt){
+		while(cnt--){
+			s.push_back(' ');
+		}
+	}
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
